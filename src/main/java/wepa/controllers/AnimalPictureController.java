2d6d9aff278a63df41b73de29a95bb0a7320e1fd
Jani Kaarela <jani.kaarela@saunalahti.com>
2014-11-03 09:34:04 +0200

    Re-added GET support. Updated index template.

--- src/main/java/wepa/controllers/AnimalPictureController.java
+++ src/main/java/wepa/controllers/AnimalPictureController.java
@@ -1,24 +1,37 @@
 package wepa.controllers;
 
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import wepa.domain.AnimalPicture;
 import wepa.repository.AnimalPictureRepository;
 
 @Controller
 @RequestMapping("/")
 public class AnimalPictureController {
-    @Autowired
+    
+	static final String INDEX_TEMPLATE = "index";
+	static final String INDEX_REDIRECT = "redirect:/";
+	
+	@Autowired
     private AnimalPictureRepository animalRepository;
     
-    @RequestMapping(value="/", method=RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.GET)
+	public String index(Model model) {
+		// TODO: add pics to model
+		return INDEX_TEMPLATE; 
+	}
+	
+    @RequestMapping(method = RequestMethod.POST)
     public String addNewAnimalPicture(@Valid @ModelAttribute AnimalPicture animalPicture){
         animalRepository.save(animalPicture);
-        
-        return "redirect:/";
+        // TODO: validation errors?
+        return INDEX_REDIRECT;
     }
 }
