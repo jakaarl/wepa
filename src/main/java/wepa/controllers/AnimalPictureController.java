c11a4023f5da6a713de99b6fcc6898edf27a9933
Jani Kaarela <jani.kaarela@gmail.com>
2014-11-04 20:40:59 +0200

    Reformatted AnimalPictureController.

--- src/main/java/wepa/controllers/AnimalPictureController.java
+++ src/main/java/wepa/controllers/AnimalPictureController.java
@@ -1,6 +1,5 @@
 package wepa.controllers;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,45 +19,40 @@ import wepa.service.AnimalPictureService;
 @Controller
 @RequestMapping("/")
 public class AnimalPictureController {
-    
-	static final String INDEX_TEMPLATE = "index";
-	static final String INDEX_REDIRECT = "redirect:/";
-	
-   
-    
-        @Autowired
-        private AnimalPictureService animalPictureService;
 
+    static final String INDEX_TEMPLATE = "index";
+    static final String INDEX_REDIRECT = "redirect:/";
 
-        @RequestMapping(method = RequestMethod.GET)
-        public String index(Model model) {
-                // TODO: add pics to model
-                return INDEX_TEMPLATE; 
+    @Autowired
+    private AnimalPictureService animalPictureService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(Model model) {
+        // TODO: add pics to model
+        return INDEX_TEMPLATE;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String addNewAnimalPicture(@RequestParam MultipartFile file, @RequestParam String description,
+            RedirectAttributes redirectAttributes, Model model) throws Exception {
+        try {
+
+            AnimalPicture picture = animalPictureService.add(file, description);
+            redirectAttributes.addFlashAttribute("message", "Your picture has been saved successfuly");
+            redirectAttributes.addFlashAttribute("id", picture.getId());
+            redirectAttributes.addFlashAttribute("description", picture.getDescription());
+            redirectAttributes.addFlashAttribute("name", picture.getName());
+            return INDEX_REDIRECT;
+        } catch (Exception ex) {
+            model.addAttribute("errorMessage", ex.getMessage());
+            return INDEX_TEMPLATE;
         }
+    }
 
-        
-        @RequestMapping(method = RequestMethod.POST)
-        public String addNewAnimalPicture(@RequestParam MultipartFile file, @RequestParam String description, 
-                                                RedirectAttributes redirectAttributes, Model model) throws Exception{      
-            try {
-               
-                AnimalPicture picture = animalPictureService.add(file,description);
-                redirectAttributes.addFlashAttribute("message", "Your picture has been saved successfuly");
-                redirectAttributes.addFlashAttribute("id", picture.getId());
-                redirectAttributes.addFlashAttribute("description", picture.getDescription());
-                redirectAttributes.addFlashAttribute("name", picture.getName());
-                return INDEX_REDIRECT;
-            } catch (Exception  ex) {
-                model.addAttribute("errorMessage", ex.getMessage());
-                return INDEX_TEMPLATE;
-            }
-        }
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> getImage(@PathVariable String id) {
 
-        @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-        public ResponseEntity<byte[]> getImage(
-                @PathVariable String id) {
+        return animalPictureService.createResponseEntity(id);
+    }
 
-            return animalPictureService.createResponseEntity(id);
-        }
-        
 }
