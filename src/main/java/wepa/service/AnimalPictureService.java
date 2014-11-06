d5eb0b05e021b83960a3891412e4af983025346d
Jani Kaarela <jani.kaarela@gmail.com>
2014-11-06 07:59:14 +0200

    Service method for getting latest pics + tests.

--- src/main/java/wepa/service/AnimalPictureService.java
+++ src/main/java/wepa/service/AnimalPictureService.java
@@ -2,8 +2,12 @@ package wepa.service;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,6 +22,18 @@ public class AnimalPictureService {
     
     public AnimalPicture getById(Long id) {
         return pictureRepo.findOne(id);
+    }
+    
+    /**
+     * Gets <code>maxCount</code> latest animal pictures, newest first.
+     * 
+     * @param maxCount  maximum number to fetch.
+     * 
+     * @return  a list of latest animal pictures.
+     */
+    public List<AnimalPicture> getLatest(int maxCount) {
+        Pageable limit = new PageRequest(0, maxCount, Direction.DESC, "added");
+        return pictureRepo.findAll(limit).getContent();
     }
     
     public AnimalPicture add(MultipartFile file, String description) throws IllegalArgumentException, IOException {
