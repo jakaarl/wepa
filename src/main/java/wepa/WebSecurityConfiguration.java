59cb4d1ace40ac44146077d1405d84e9effba70c
Jani Kaarela <jani.kaarela@gmail.com>
2014-11-09 08:41:59 +0200

    Fixing build/tests and Heroku deployment.
    * Procfile shouldn't contain build command after all?
    * corrected Spring Security dependency
    * allow all, for now

---
+++ src/main/java/wepa/WebSecurityConfiguration.java
@@ -0,0 +1,26 @@
+package wepa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        // allow everything, for now
+        security.authorizeRequests().anyRequest().permitAll();
+    }
+    
+    /* TODO: configure authentication
+    @Configuration
+    protected static class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
+        
+    }
+    */
+
+}
