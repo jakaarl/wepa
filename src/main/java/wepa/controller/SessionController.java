c2de90162a135423b926bdf55441ec07001bff05
Kristian Lauttamus <krizzuq@hotmail.com>
2014-11-16 22:48:01 +0200

    Registration form and some template cleaning

---
+++ src/main/java/wepa/controller/SessionController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wepa.controller;
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import wepa.domain.User;
+import wepa.service.UserService;
+
+@Controller
+public class SessionController {
+    @Autowired
+    UserService userService;
+    
+    @RequestMapping(value = "/register")
+    public String getRegister(){
+        return "register";
+    }
+    
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String postRegister(@Valid @ModelAttribute User user, 
+                            RedirectAttributes redirectAttributes){
+        User created = userService.save(user);
+        
+        if(created == null){
+            redirectAttributes.addFlashAttribute("errorMessage", "User was not created");
+            return "redirect:/register";
+        }
+        
+        redirectAttributes.addFlashAttribute("message", "User created");
+        return "redirect:/";
+    }
+}
--- src/main/java/wepa/service/UserService.java
+++ src/main/java/wepa/service/UserService.java
@@ -13,4 +13,13 @@ public class UserService {
     public User findUserByEmail(String email){
         return userRepository.findByEmail(email);
     }
+    
+    public User save(User user) {
+        if (user.getEmail()==null || 
+                user.getFirstName()==null || 
+                user.getLastName()==null){
+            throw new IllegalArgumentException("User email, first- or lastname must not be empty!");
+        }
+        return userRepository.save(user);
+    }
 }
