dbf1a3344eeb956ce0a51a6a6039961675cf9343
Jani Kaarela <jani.kaarela@saunalahti.com>
2014-11-18 17:57:45 +0200

    Added some hard-coded paths/redirects to Routes.

--- src/main/java/wepa/helpers/Routes.java
+++ src/main/java/wepa/helpers/Routes.java
@@ -10,11 +10,16 @@ public class Routes {
     // Redirects
     public static final String INDEX_REDIRECT = "redirect:/";
     public static final String ALBUMS_REDIRECT = "redirect:/albums/";
+    public static final String LOGIN_REDIRECT = "redirect:/login";
+    public static final String REGISTER_REDIRECT = "redirect:/register";
     
-    // Templates
-    public static final String INDEX_TEMPLATE = "index";
-    public static final String ALBUMS_TEMPLATE = "albums";
+    // Templates    
     public static final String ALBUM_TEMPLATE = "album";
-    public static final String PROFILE_TEMPLATE = "profile";
+    public static final String ALBUMS_TEMPLATE = "albums";
     public static final String PICTURE_TEMPLATE = "image";
+    public static final String INDEX_TEMPLATE = "index";
+    public static final String LOGIN_TEMPLATE = "login";
+    public static final String PROFILE_TEMPLATE = "profile";
+    public static final String REGISTER_TEMPLATE = "register";
+    
 }
