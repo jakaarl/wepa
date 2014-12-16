97688baca7ce68983ce205097bc3b28e658f0884
Jani Kaarela <jani.kaarela@wunderdog.fi>
2014-12-16 22:24:57 +0200

    Controllers cleanup.

--- src/main/java/wepa/controller/AlbumController.java
+++ src/main/java/wepa/controller/AlbumController.java
@@ -9,7 +9,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
@@ -17,8 +16,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import wepa.controller.parameter.AnimalPictureFile;
@@ -28,7 +25,6 @@ import wepa.helpers.CurrentUserProvider;
 import wepa.helpers.Routes;
 import wepa.service.AlbumService;
 import wepa.service.AnimalPictureService;
-
 
 @Controller
 @RequestMapping("/albums/")
@@ -43,9 +39,8 @@ public class AlbumController {
     @Autowired
     private CurrentUserProvider currentUserProvider;
 
-    // Get albums
     @RequestMapping(method = RequestMethod.GET)
-    public String index(Model model) {
+    public String getAlbums(Model model) {
         model.addAttribute("albums", albumService.getAll());
         return Routes.ALBUMS_TEMPLATE;
     }
@@ -57,20 +52,21 @@ public class AlbumController {
         try {
             albumService.save(album);
             redirectAttributes.addFlashAttribute("message", "New album has been created!");
+            redirectAttributes.addAttribute("id", album.getId());
         } catch (IllegalArgumentException e){
             redirectAttributes.addFlashAttribute("error", e.getMessage());
             redirectAttributes.addFlashAttribute("description", album.getDescription());
             return "redirect:/albums/#submitAlbumModal";
         }
         
-        return "redirect:/albums/" + album.getId();
+        return Routes.ALBUM_BY_ID_REDIRECT;
     }
     
     @ModelAttribute("album")
     private Album getAlbum() {
         return new Album();
     }
-    // Get Album by id
+    
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public String getAlbum(@PathVariable Long id, Model model) {
         Album album= albumService.find(id);
--- src/main/java/wepa/controller/IndexController.java
+++ src/main/java/wepa/controller/IndexController.java
@@ -1,19 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package wepa.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import wepa.domain.Album;
 import wepa.domain.AnimalPicture;
 import wepa.helpers.Routes;
@@ -24,8 +18,8 @@ import wepa.service.AnimalPictureService;
 @RequestMapping("/")
 public class IndexController {
     // How many items per load
-    private final int albumsPerPage = 2;
-    private final int animalPicturesPerPage = 6;
+    private static final int ALBUMS_PER_PAGE = 2;
+    private static final int ANIMAL_PICTURES_PER_PAGE = 6;
     
     @Autowired
     AlbumService albumService;
@@ -35,27 +29,27 @@ public class IndexController {
     
     @RequestMapping
     public String index(Model model){
-        List<AnimalPicture> animalPictures = animalPictureService.getLatest(animalPicturesPerPage);
-        if(animalPictures.size() > animalPicturesPerPage / 2){
-            List<AnimalPicture> firstPictures = animalPictures.subList(0, animalPicturesPerPage/2);
-            List<AnimalPicture> secondPictures = animalPictures.subList(animalPicturesPerPage/2, animalPictures.size());
+        List<AnimalPicture> animalPictures = animalPictureService.getLatest(ANIMAL_PICTURES_PER_PAGE);
+        if(animalPictures.size() > ANIMAL_PICTURES_PER_PAGE / 2){
+            List<AnimalPicture> firstPictures = animalPictures.subList(0, ANIMAL_PICTURES_PER_PAGE/2);
+            List<AnimalPicture> secondPictures = animalPictures.subList(ANIMAL_PICTURES_PER_PAGE/2, animalPictures.size());
             model.addAttribute("firstPictures",firstPictures);
             model.addAttribute("secondPictures",secondPictures);
         } else {
             model.addAttribute("firstPictures",animalPictures);
         }
         
-        List<Album> albums = albumService.getLatest(albumsPerPage);
-        if(albums.size() > albumsPerPage / 2){
-            List<Album> firstAlbums = albums.subList(0, albumsPerPage/2);
-            List<Album> secondAlbums = albums.subList(albumsPerPage/2, albums.size());
+        List<Album> albums = albumService.getLatest(ALBUMS_PER_PAGE);
+        if(albums.size() > ALBUMS_PER_PAGE / 2){
+            List<Album> firstAlbums = albums.subList(0, ALBUMS_PER_PAGE/2);
+            List<Album> secondAlbums = albums.subList(ALBUMS_PER_PAGE/2, albums.size());
             model.addAttribute("firstAlbums", firstAlbums);
             model.addAttribute("secondAlbums", secondAlbums);
         } else {
             model.addAttribute("firstAlbums", albums);
         }
         
-        if(albumService.countAlbums() > albumsPerPage || animalPictureService.countAnimalPictures() > animalPicturesPerPage){
+        if(albumService.countAlbums() > ALBUMS_PER_PAGE || animalPictureService.countAnimalPictures() > ANIMAL_PICTURES_PER_PAGE){
             model.addAttribute("nextPage", 2);
         }
         return Routes.INDEX_TEMPLATE;
@@ -63,27 +57,27 @@ public class IndexController {
     
     @RequestMapping("{page}")
     public String loadMore(@PathVariable int page, Model model){
-        List<AnimalPicture> animalPictures = animalPictureService.getLatest(page*animalPicturesPerPage, page*(animalPicturesPerPage*2));
-        if(animalPictures.size() > animalPicturesPerPage / 2){
-            List<AnimalPicture> firstPictures = animalPictures.subList(0, animalPicturesPerPage/2);
-            List<AnimalPicture> secondPictures = animalPictures.subList(animalPicturesPerPage/2, animalPictures.size());
+        List<AnimalPicture> animalPictures = animalPictureService.getLatest(page*ANIMAL_PICTURES_PER_PAGE, page*(ANIMAL_PICTURES_PER_PAGE*2));
+        if(animalPictures.size() > ANIMAL_PICTURES_PER_PAGE / 2){
+            List<AnimalPicture> firstPictures = animalPictures.subList(0, ANIMAL_PICTURES_PER_PAGE/2);
+            List<AnimalPicture> secondPictures = animalPictures.subList(ANIMAL_PICTURES_PER_PAGE/2, animalPictures.size());
             model.addAttribute("firstPictures",firstPictures);
             model.addAttribute("secondPictures",secondPictures);
         } else {
             model.addAttribute("firstPictures",animalPictures);
         }
         
-        List<Album> albums = albumService.getLatest(page*albumsPerPage, page*(albumsPerPage*2));
-        if(albums.size() > albumsPerPage / 2){
-            List<Album> firstAlbums = albums.subList(0, albumsPerPage/2);
-            List<Album> secondAlbums = albums.subList(albumsPerPage/2, albums.size());
+        List<Album> albums = albumService.getLatest(page*ALBUMS_PER_PAGE, page*(ALBUMS_PER_PAGE*2));
+        if(albums.size() > ALBUMS_PER_PAGE / 2){
+            List<Album> firstAlbums = albums.subList(0, ALBUMS_PER_PAGE/2);
+            List<Album> secondAlbums = albums.subList(ALBUMS_PER_PAGE/2, albums.size());
             model.addAttribute("firstAlbums", firstAlbums);
             model.addAttribute("secondAlbums", secondAlbums);
         } else {
             model.addAttribute("firstAlbums", albums);
         }
         
-        if(albumService.countAlbums() > page*(albumsPerPage*2) || animalPictureService.countAnimalPictures() > page*(animalPicturesPerPage*2)){
+        if(albumService.countAlbums() > page*(ALBUMS_PER_PAGE*2) || animalPictureService.countAnimalPictures() > page*(ANIMAL_PICTURES_PER_PAGE*2)){
             model.addAttribute("nextPage", page+1);
         }
         return Routes.INDEX_TEMPLATE;
