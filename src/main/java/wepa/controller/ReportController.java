c9cad1ad11384a41f184d08127f35063d89d940c
Jani Kaarela <jani.kaarela@wunderdog.fi>
2014-12-19 00:57:59 +0200

    Added auth annotations to ReportController.

--- src/main/java/wepa/controller/ReportController.java
+++ src/main/java/wepa/controller/ReportController.java
@@ -1,14 +1,18 @@
 package wepa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import wepa.domain.AlbumReport;
 import wepa.domain.AnimalPictureReport;
 import wepa.domain.CommentReport;
@@ -21,12 +25,17 @@ public class ReportController {
     @Autowired
     ReportService reportService;
     
+    @InitBinder
+    private void initBinder(WebDataBinder binder) {
+        binder.setDisallowedFields("sentBy");
+    }
+    
     /**
      * 
      *    CommentReports
      * 
      */
-    
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "comments", method = RequestMethod.GET)
     public String getCommentReports(Model model){
         model.addAttribute("commentReports", reportService.getCommentReports());
@@ -34,6 +43,7 @@ public class ReportController {
         return Routes.COMMENT_REPORTS_TEMPLATE;
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "comments/{id}", method = RequestMethod.GET)
     public String getCommentReport(@PathVariable Long id, RedirectAttributes redirectAttributes, Model model){
         CommentReport commentReport = reportService.getCommentReport(id);
@@ -48,6 +58,7 @@ public class ReportController {
         return Routes.COMMENT_REPORT_TEMPLATE;
     }
     
+    @PreAuthorize("authenticated")
     @ResponseBody
     @RequestMapping(value = "comments/{id}", method = RequestMethod.POST)
     public String postCommentReport(@PathVariable Long id, @RequestBody CommentReport commentReport){
@@ -56,6 +67,7 @@ public class ReportController {
         return "saved";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "comments/{id}", method = RequestMethod.DELETE)
     public String postCommentReport(@PathVariable Long id){
         reportService.deleteCommentReport(id);
@@ -68,7 +80,7 @@ public class ReportController {
      *    AlbumReports
      * 
      */
-    
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "albums", method = RequestMethod.GET)
     public String getAlbumReports(Model model){
         model.addAttribute("albumReports", reportService.getAlbumReports());
@@ -76,6 +88,7 @@ public class ReportController {
         return Routes.ALBUM_REPORTS_TEMPLATE;
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "albums/{id}", method = RequestMethod.GET)
     public String getAlbumReport(@PathVariable Long id, RedirectAttributes redirectAttributes, Model model){
         AlbumReport albumReport = reportService.getAlbumReport(id);
@@ -90,6 +103,7 @@ public class ReportController {
         return Routes.ALBUM_REPORTS_TEMPLATE;
     }
     
+    @PreAuthorize("authenticated")
     @ResponseBody
     @RequestMapping(value = "albums/{id}", method = RequestMethod.POST)
     public String postAlbumReport(@PathVariable Long id, @RequestBody AlbumReport albumReport){
@@ -98,6 +112,7 @@ public class ReportController {
         return "saved";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "albums/{id}", method = RequestMethod.DELETE)
     public String postAlbumReport(@PathVariable Long id){
         reportService.deleteAlbumReport(id);
@@ -110,7 +125,7 @@ public class ReportController {
      *    AnimalPictureReports
      * 
      */
-    
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "animalpictures", method = RequestMethod.GET)
     public String getAnimalPictureReports(Model model){
         model.addAttribute("animalPictureReports", reportService.getAnimalPictureReports());
@@ -118,6 +133,7 @@ public class ReportController {
         return Routes.ANIMALPICTURE_REPORTS_TEMPLATE;
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "animalpictures/{id}", method = RequestMethod.GET)
     public String getAnimalPictureReport(@PathVariable Long id, RedirectAttributes redirectAttributes, Model model){
         AnimalPictureReport animalPictureReport = reportService.getAnimalPictureReport(id);
@@ -132,6 +148,7 @@ public class ReportController {
         return Routes.ANIMALPICTURE_REPORTS_TEMPLATE;
     }
     
+    @PreAuthorize("authenticated")
     @ResponseBody
     @RequestMapping(value = "animalpictures/{id}", method = RequestMethod.POST)
     public String postAnimalPictureReport(@PathVariable Long id, @RequestBody AnimalPictureReport animalPictureReport){
@@ -140,6 +157,7 @@ public class ReportController {
         return "saved";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "animalpictures/{id}", method = RequestMethod.DELETE)
     public String postAnimalPictureReport(@PathVariable Long id){
         reportService.deleteAnimalPictureReport(id);
